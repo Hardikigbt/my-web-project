21fffecb8df85646dfadaf5755a0823f2646cf7f
Hardik <109631616+Hardikigbt@users.noreply.github.com>
2023-07-25 22:49:18 -0400

    .

---
+++ Python.py
@@ -0,0 +1,14 @@
+print ("Hello Python")
+a= int (input("Please eneter a:----"))
+b= int (input("Please eneter b:----"))
+if a <= 10:
+    print("hello")
+elif b>= 10:
+    print ("Whooo")
+sal = input ("Please neter your salary:____")
+
+if sal >=99:
+    print("saras")
+elif sal <=100:
+    print("not good")
+    
